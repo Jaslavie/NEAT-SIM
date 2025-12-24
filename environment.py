919e18543e5b1f8c99d5894d807fd941eb2c4e1b
Jaslavie <jasmineqw45@gmail.com>
2025-12-24 02:04:53 -0800

    implemented basic evolution loop + simple action space

--- environment.py
+++ environment.py
@@ -1,15 +1,16 @@
+
 class WargameEnv:
     def __init__(self):
         # unit states
         self.blue_units=27
-        self.blue_pos=[100,100]
-        self.blue_health=1.0
+        self.blue_pos = [200, 200]
+        self.blue_hp = 1.0
         self.blue_firepower = 0.8
 
         self.red_units=33
-        self.red_pos = [0,0]
+        self.red_pos  = [400, 200]
+        self.red_hp  = 1.0
         self.red_firepower = 0.5
-        self.red_health = 1.0
 
         # global states
         self.time = 0
@@ -22,28 +23,65 @@ class WargameEnv:
         """
         reset the simulation after each run
         """
+        self.__init__()
     
     def step(self, action):
         """
-        execute on a series of actions
+        execute on an action given an activation number
+        - action: resulting actions of forward pass (array of activation floats of each output node)
         """
+        if action[0] > 0.5:
+            self.blue_pos = self.move_forward(self.blue_pos)
+        if action[1] > 0.5:
+            self.blue_pos = self.move_backward(self.blue_pos)
+        if action[2] > 0.5:
+            if self.red_units > 0:
+                damage = self.calculate_damage(self.blue_units, self.blue_firepower)
+                self.red_units -= damage
+                self.red_units = max(0, self.red_units) # prevent units from reaching negative
+        if action[3] > 0.5:
+            pass # hold/defend
+        
+    
     def get_observation(self):
         """
         get the current state of env as input in the NN
         """
         # return nodes for neural net
         return {
-            1: self.blue_pos[0], # blue x position
-            2: self.blue_pos[1], # blue y position
-            3: self.red_pos[0],
-            4: self.red_pos[1],
+            1: self.blue_units,
+            2: self.red_units,
 
-            5: self.wind_speed,
+            3: self.wind_speed,
 
-            6: self.blue_health,
-            7: self.red_health,
-            8: self.blue_firepower,
-            9: self.red_firepower,
-            10: self.blue_units,
-            11: self.red_units,
+            # 4: self.blue_hp,
+            # 5: self.red_hp,
+            4: self.blue_firepower,
+            5: self.red_firepower,
         }
+    
+    # ======== ACTIONS ==========
+    def move_forward(self, blue_pos):
+        """
+        advance
+        move right on the x axis
+        """
+        blue_pos[0] += 10
+        return blue_pos
+
+    def move_backward(self, blue_pos):
+        """
+        retreat
+        move left on the x axis
+        """
+        blue_pos[0] -= 10
+        return blue_pos
+    
+    def calculate_damage(self, blue_units, blue_firepower):
+        """
+        damage computed from lanchester equation
+        \(dB/dt=-aA\)
+        """
+        return blue_units * blue_firepower
+    
+    # def interpret_actions(action):
--- game.py
+++ game.py
@@ -3,21 +3,49 @@ import pyglet
 from pyglet import shapes
 
 class Game:
+    _window = None # set global variable for window
+
     def __init__(self, env: WargameEnv):
         """
         initialize state of the world once for the entire game
         """
         self.env = env
-        self.window = pyglet.window.Window()
+        if Game._window is None:
+            Game._window = pyglet.window.Window()
+        self.window = Game._window
         self.batch = pyglet.graphics.Batch()
 
+        # initialize blue and red forces
         self.blue = pyglet.shapes.Circle(
-            x = self.env.blue_pos[0],
-            y = self.env.blue_pos[1],
-            radius = 20, 
-            color=(0, 0, 255),
+            x = env.blue_pos[0],
+            y = env.blue_pos[1],
+            radius = 20,
+            color = (0, 0, 255),
             batch=self.batch
         )
+        self.red = pyglet.shapes.Circle(
+            x = env.red_pos[0],
+            y = env.red_pos[1],
+            radius = 20,
+            color = (255, 0, 0),
+            batch=self.batch
+        )
+        # blue_objs = self.env.blue_pos.values()
+        # self.blue_circles = {}
+
+        # for blue_obj in blue_objs:
+        #     position = self.env.blue_pos[blue_obj]
+        #     x, y = position
+
+        #     circle = pyglet.shapes.Circle(
+        #         x = x,
+        #         y = y,
+        #         radius = 20, 
+        #         color=(0, 0, 255),
+        #         batch=self.batch
+        #     )
+
+            # self.blue_circles[blue_obj] = circle
 
         @self.window.event
         def on_draw():
@@ -25,9 +53,17 @@ class Game:
             self.batch.draw()
 
     def render(self):
-        # initialize object positions in display
+        # initialize all object positions in display
+        # for uid, pos in self.env.blue_pos.items():
+        #     circle = self.blue_circles[uid]
+        #     x, y = pos
+        #     circle.x = x
+        #     circle.y = y
         self.blue.x = self.env.blue_pos[0]
         self.blue.y = self.env.blue_pos[1]
+
+        self.red.x = self.env.red_pos[0]
+        self.red.y = self.env.red_pos[1]
 
         # main rendering
         pyglet.clock.tick()
