a4996a634593faff4487d67b429e00e59ef9b8b0
Jaslavie <jasmineqw45@gmail.com>
2025-12-31 10:31:58 +0900

    feat: implemented line formation state

--- environment.py
+++ environment.py
@@ -3,7 +3,7 @@ from abc import ABC, abstractmethod
 
 class WargameEnv:
     def __init__(self):
-        # unit states
+        # unit counts
         self.blue_units=27
         self.blue_pos = [500, 500]
         self.blue_hp = 1.0
@@ -21,6 +21,8 @@ class WargameEnv:
         # env states
         self.wind_speed=0.3
 
+        self.red_fsm = RedStateMachine()
+
     def reset(self):
         """
         reset the simulation after each run
@@ -33,7 +35,80 @@ class WargameEnv:
         intensity of action is determined separately if activated
         - action: resulting actions of forward pass (array of activation floats of each output node)
         """
-        # only pick action with highest activation
+        self.get_blue_action(action)
+
+        # counter attacks from red (only if red has units and blue has units)
+        if self.red_units > 0 and self.blue_units > 0:
+            red_damage = self.get_red_actions(self.blue_pos, self.red_pos)
+            self.blue_units -= red_damage
+            self.blue_units = max(0, int(round(self.blue_units)))  # ensure units remain integer
+        
+        # increment time
+        self.time += 1
+
+        # terminate condition
+        done = self.is_done()
+        # reward = self.calculate_reward(done) # calc the reward at the end state
+        return self.get_observation(), done
+
+    def get_observation(self):
+        """
+        get the current state of env as input in the NN
+        """
+        # return nodes for neural net
+        return {
+            1: self.blue_units,
+            2: self.red_units,
+
+            3: self.wind_speed,
+
+            # 4: self.blue_hp,
+            # 5: self.red_hp,
+            4: self.blue_firepower,
+            5: self.red_firepower,
+        }
+    
+    def check_victory(self):
+        """
+        victory condition is based on the lanchester function
+        """
+        blue_combat_power = self.blue_firepower * (self.blue_units ** 2)
+        red_combat_power = self.red_firepower * (self.red_units ** 2)
+
+        # A wins if:\(\alpha A_{0}>\beta B_{0}\)\
+        if blue_combat_power > red_combat_power:
+            return "blue_advantage"
+        elif red_combat_power > blue_combat_power:
+            return "red_advantage"
+        return "even"
+    
+    def is_done(self):
+        """
+        check if simulation should terminate early
+        """
+        return (self.blue_units <= 0 or self.red_units <= 0 or self.time >= self.max_time)
+
+    # ======== ACTIONS ========== 
+    def calculate_damage(self, units, firepower, hit_probability=0.6):
+        """
+        damage computed from lanchester equation
+        \\(dB/dt=-aA\\)
+        """
+        damage_rate = units * firepower
+
+        # determine if units are hit at all, then calculate damage
+        if random.random() < hit_probability:
+            actual_damage = damage_rate * random.uniform(0, 1.0)  # number of units destroyed
+        else:
+            actual_damage = 0
+        
+        return actual_damage
+    
+    def get_blue_action(self, action):
+        """
+        potential actions selected by NEAT algorithm
+        """
+        # compute highest probability action
         max_indx = action.index(max(action))
 
         # engage
@@ -62,118 +137,51 @@ class WargameEnv:
         if max_indx == 3:
             pass
 
-        # counter attacks from red (only if red has units and blue has units)
-        if self.red_units > 0 and self.blue_units > 0:
-            red_damage = self.get_red_actions(self.blue_pos, self.red_pos)
-            self.blue_units -= red_damage
-            self.blue_units = max(0, int(round(self.blue_units)))  # ensure units remain integer
+    def get_red_actions(self):
+        """
+        selected from red state machine
+        """
+        pass
         
-        # increment time
-        self.time += 1
-
-        # terminate condition
-        done = self.is_done()
-        # reward = self.calculate_reward(done) # calc the reward at the end state
-        return self.get_observation(), done
-        
-    
-    def get_observation(self):
-        """
-        get the current state of env as input in the NN
-        """
-        # return nodes for neural net
-        return {
-            1: self.blue_units,
-            2: self.red_units,
-
-            3: self.wind_speed,
-
-            # 4: self.blue_hp,
-            # 5: self.red_hp,
-            4: self.blue_firepower,
-            5: self.red_firepower,
-        }
-    
-    def check_victory(self):
-        """
-        victory condition is based on the lanchester function
-        """
-        blue_combat_power = self.blue_firepower * (self.blue_units ** 2)
-        red_combat_power = self.red_firepower * (self.red_units ** 2)
-
-        # A wins if:\(\alpha A_{0}>\beta B_{0}\)
-        if blue_combat_power > red_combat_power:
-            return "blue_advantage"
-        elif red_combat_power > blue_combat_power:
-            return "red_advantage"
-        return "even"
-    
-    def is_done(self):
-        """
-        check if simulation should terminate early
-        """
-        return (self.blue_units <= 0 or self.red_units <= 0 or self.time >= self.max_time)
-
-    # ======== ACTIONS ========== 
-    def calculate_damage(self, units, firepower, hit_probability=0.6):
-        """
-        damage computed from lanchester equation
-        \(dB/dt=-aA\)
-        """
-        damage_rate = units * firepower
-
-        # determine if units are hit at all, then calculate damage
-        if random.random() < hit_probability:
-            actual_damage = damage_rate * random.uniform(0, 1.0)  # number of units destroyed
-        else:
-            actual_damage = 0
-        
-        return actual_damage
-    
-    def get_red_actions(self, blue_pos, red_pos):
-        # compute on x axis
-        distance = abs(blue_pos[0] - red_pos[0])
-
-        red_strength_ratio = self.red_units / 33.0
-        
-        # red ships will only attack upon close range
-        if distance > 100:
-            red_effective_units = self.red_units * 0.3
-        elif distance < 50:
-            red_effective_units = self.red_units * 0.8
-        else:
-            red_effective_units = self.red_units * 0.5 
-        
-        red_damage = self.calculate_damage(red_effective_units, self.red_firepower)
-
-        return red_damage
     
     # def interpret_actions(action):
 
 class StateMachine(ABC):
     def __init__(self, name: str):
-    
+        self.name = name
     # ==== lifecycle hooks to define current state ====
     def on_enter(self, obs):
+        print(f"entering {self.name}")
+        pass
     def on_update(self, obs):
+        pass
     def on_exit(self, obs):
+        print(f"exiting {self.name}")
+        pass
 
     # ==== hfsm management ====
-    def load_substate()
+    def load_substate(self):
         """
         substates are strategic positions: defend, attack, etc
         """
-    def add_transition()
+        pass
+    def add_transition(self):
+        pass
 
-    def enter_state_machine()
+    def enter_state_machine():
+        pass
 
-    def update_state_machine()
+    def update_state_machine():
+        pass
 
-    def exit_state_machine()
+    def exit_state_machine():
+        pass
 
     # implemented by children
     @abstractmethod
     def select_action(self, obs) -> dict:
+        pass
+
 
 class RedStateMachine(StateMachine):
     def __init__(self):
@@ -189,44 +197,95 @@ class RedStateMachine(StateMachine):
         self.current_state = self.states['LINE']
         self.current_state.enter_state_machine({})
 
+        # collect history of actions
+        self.action_history = []
+
     def update(self, obs):
-        
-    def transition(state):
+        """
+        update red's actions for each step
+        """
+        # check if we need to transition to a new state
+        new_state = self.check_transition(obs)
+        if new_state and new_state != self.current_state:
+            print(f"transitioned to {new_state}")
+            self.transition_to(new_state)
+
+        # execute action based on current state
+        action = self.current_state.select_action(obs)
+
+        return action
+    
+    def check_transition(self, obs):
+        """
+        check if observations trigger a strategy state transition
+        """
+        pass
+
+    def transition_to(self, new_state: str):
+        """
+        execute transitions to diff states
+        """
+        print(f"red fleet: {self.current_state} transitioned to {new_state}")
+
+        # switch to new state
+        self.current_state = self.states[new_state]
 
     
 class LineFormState:
     """
     maintain position with controlled fire
     """
-    def on_enter(self, obs):
-
-    def on_update(self, obs):
-    
-    def on_exit(self, obs):
 
     def select_action(self, obs):
+        """
+        distance based actions
+        - long range = minimal fire
+        - med range = controlled fire
+        - close range: transition to different state
+        """
+        # compute on x axis
+        distance = abs(obs['blue_pos'][0] - obs['red_pos'][0])
+
+        red_strength_ratio = obs['red_units'] / 33.0
+        
+        # red ships will only attack upon close range
+        if distance > 100:
+            type = 'LONG_RANGE_FIRE'
+            red_effective_units = obs['red_units'] * 0.3
+        elif distance < 50:
+            type = 'CLOSE_RANGE_FIRE'
+            red_effective_units = obs['red_units'] * 0.8
+        else:
+            type = 'CONTROLLED_FIRE'
+            red_effective_units = obs['red_units'] * 0.5 
+        
+        return {
+            'type': type,
+            'effective_units': red_effective_units
+        }
 
 class DefenseState:
     """
     franco-spanish ships fragment after the british breaks the line
     """
-    def on_enter(self, obs):
-
-    def on_update(self, obs):
-    
-    def on_exit(self, obs):
-
     def select_action(self, obs):
+        pass
 
 class RetreatState:
     """
     full retreat to cadiz port
     """
-    def on_enter(self, obs):
-
-    def on_update(self, obs):
-    
-    def on_exit(self, obs):
-
     def select_action(self, obs):
-            
+        pass
+
+test_obs = {
+    'blue_pos': [890, 500],
+    'red_pos': [800, 500],
+    'red_units': 33,
+    'red_firepower': 0.5,
+    'blue_units': 27,
+    'blue_firepower': 0.8
+}
+state = LineFormState()
+action = state.select_action(test_obs)
+print(action)
